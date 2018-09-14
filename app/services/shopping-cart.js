7e67127b818619f2170995bb9c0f30eebac487fc
Jaineshkumar Patel <jenu.16@icloud.com>
2018-09-14 05:08:29 -0500

    Handling user actions with ember

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  cart: [],
+  addToCart(color, size, price) {
+    this.cart.push({
+      itemColor: color,
+      itemSize: size,
+      itemPrice: price
+    });
+  },
+  getCartItems() {
+    return this.get('cart');
+  }
 });
